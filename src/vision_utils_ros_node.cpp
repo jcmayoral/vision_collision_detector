b459f6f796396b81a103839504eb0e851c688da8
jcmayoral <jocamaba1989@gmail.com>
2017-07-14 19:17:34 +0200

    test start run stop

--- src/vision_utils_ros_node.cpp
+++ src/vision_utils_ros_node.cpp
@@ -5,7 +5,9 @@ using namespace std;
 
 int main(int argc, char *argv[]){
     FaultDetection fd;
+    fd.start();
     fd.run();
+    fd.stop();
     std::cout << "inside" <<std::endl;
     return 0;
 }
