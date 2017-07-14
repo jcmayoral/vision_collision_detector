2e5fa9e5a1684caec02967663b7a951d07c26fa6
jcmayoral <jocamaba1989@gmail.com>
2017-07-14 19:08:23 +0200

    at least it compiles with some strange error

--- src/vision_utils_ros_node.cpp
+++ src/vision_utils_ros_node.cpp
@@ -1,7 +1,11 @@
 #include <stdio.h>
+#include <featuredetection/FD.h>
 
 using namespace std;
 
-int main(int, char **){
-
+int main(int argc, char *argv[]){
+    FaultDetection fd;
+    fd.run();
+    std::cout << "inside" <<std::endl;
+    return 0;
 }
