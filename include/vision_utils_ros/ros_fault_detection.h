ed6c0ce68f3f96721d7c0e4004c9ff958c08d6c1
jcmayoral <jocamaba1989@gmail.com>
2017-07-19 18:12:00 +0200

    cv_bridge starting implementing and mutex::lock and unlock (not proceed if newer frame is not received)

--- include/vision_utils_ros/ros_fault_detection.h
+++ include/vision_utils_ros/ros_fault_detection.h
@@ -1,5 +1,8 @@
 #include <ros/ros.h>
+#include <cv_bridge/cv_bridge.h>
 #include <featuredetection/FD.h>
+#include <mutex>
+
 using namespace cv;
 using namespace std;
 
@@ -12,11 +15,6 @@ void Matcher::show(std::string window_name){
 }
 
 void MyFeatureExtractor::read(VideoCapture v){
-	cout << "my new read" << endl;
-}
-
-bool FaultDetection::run(){
-  return true;
 }
 
 bool FaultDetection::stop(){
@@ -31,13 +29,36 @@ class ROSFaultDetection:public FaultDetection{
 	public:
 		ROSFaultDetection(ros::NodeHandle nh) : FaultDetection(true){
 		}
+
+		Mat setFrame(){
+			return Mat();
+		}
 		void startDetection(){
 		  start();
 		  while(ros::ok()){
+				mtx_.lock(); // not proceed until new frame is received
 		    run();
 		  }
+
 		  stop();
 		}
+
+		void imageCb(const sensor_msgs::ImageConstPtr& msg){
+			//mtx_.lock();
+			cv_bridge::CvImagePtr cv_ptr;
+			try{
+				cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+			}
+			catch (cv_bridge::Exception& e){
+				ROS_ERROR("cv_bridge exception: %s", e.what());
+				return;
+		  }
+			first_.setFrame(cv_ptr->image);
+			mtx_.unlock();
+			//frame_ = cv_ptr->image;
+	 }
+	private:
+		std::mutex mtx_;
 };
 
 #endif /* ROSFAULTDETECTION_H_ */
