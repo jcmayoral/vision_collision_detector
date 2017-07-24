8e9e351ff0294f87c150fbb18e1ff8f8321a9927
jcmayoral <jocamaba1989@gmail.com>
2017-07-24 20:09:05 +0200

    rreturning to inheritance and overriding apparently working

--- include/vision_utils_ros/ros_fault_detection.h
+++ include/vision_utils_ros/ros_fault_detection.h
@@ -8,11 +8,28 @@ using namespace std;
 #ifndef ROSFAULTDETECTION_H_
 #define ROSFAULTDETECTION_H_
 
-class ROSFaultDetection{
+void MyFeatureExtractor::read(VideoCapture v){
+}
+
+bool FaultDetection::stop() {
+	return true;
+}
+
+bool FaultDetection::start() {
+	ROS_INFO("New Start");
+	return true;
+}
+
+bool FaultDetection::run() {
+	return true;
+}
+
+
+class ROSFaultDetection: FaultDetection{
 	public:
-		ROSFaultDetection(ros::NodeHandle nh) : is_First_Image_received(false){
+
+		ROSFaultDetection(ros::NodeHandle nh) : FaultDetection(true), is_First_Image_received(false){
 			ROS_INFO("ROSFaultDetection Constructor");
-			fd_ = FaultDetection(true);
 			image_sub_ = nh.subscribe("/camera", 1, &ROSFaultDetection::imageCb,this);
 			ros::spin();
 		}
@@ -23,24 +40,13 @@ class ROSFaultDetection{
 		void Matcher::show(std::string window_name){
 			cout << "New Show";
 		}
-
-		void MyFeatureExtractor::read(VideoCapture v){
-		}
-
-		bool FaultDetection::stop(){
-		  return true;
-		}
-
-		bool FaultDetection::start(){
-			return true;
-		}
 */
 		void startDetection(){
 			ROS_INFO("startDetection");
-		  //start();
+		  start();
 			ROS_INFO("runDetection");
 			//cv::imshow("hola", first_.frame_);
-			fd_.run();
+			run();
 		  //stop();
 		}
 
@@ -55,7 +61,10 @@ class ROSFaultDetection{
 				ROS_ERROR("cv_bridge exception: %s", e.what());
 				return;
 		  }
-			fd_.first_.setFrame(cv_ptr->image);
+			first_.setFrame(cv_ptr->image);
+			//cv::imshow("aaaaaaa", cv_ptr->image);
+			cv:waitKey(1000);
+
 
 			if (is_First_Image_received){
 				startDetection();
@@ -64,7 +73,6 @@ class ROSFaultDetection{
 	private:
 		bool is_First_Image_received;
 		ros::Subscriber image_sub_;
-		FaultDetection fd_;
 };
 
 #endif /* ROSFAULTDETECTION_H_ */
